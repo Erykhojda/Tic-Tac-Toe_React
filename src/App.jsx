74d126a6786a648aa37ee056d14781e977239e79
Eryk <erykhojda1@wp.pl>
2024-01-06 17:24:58 +0100

    1. Concept repetition: Working with State 2. Component Instaces Work in Isolation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+import Player from "./components/Player"
 
 function App() {
+  const [userName, setUserName] = useState('')
 
-
-  return
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        Game board
+      </div>
+      LOG
+    </main>
+  )
 }
 
 export default App
